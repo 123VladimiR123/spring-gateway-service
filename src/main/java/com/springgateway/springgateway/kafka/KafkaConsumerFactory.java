34d9f783d0897138088c0fdfa6cdecb7ccdbc014
123VladimiR123 <888prikol888@gmail.com>
2024-04-23 18:08:21 +0300

    fix kafka group id?

--- src/main/java/com/springgateway/springgateway/kafka/KafkaConsumerFactory.java
+++ src/main/java/com/springgateway/springgateway/kafka/KafkaConsumerFactory.java
@@ -25,7 +25,7 @@ public class KafkaConsumerFactory {
         map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        map.put(ConsumerConfig.GROUP_ID_CONFIG, "default");
+        map.put(ConsumerConfig.GROUP_ID_CONFIG, "default-gateway-consumer");
         return new DefaultKafkaConsumerFactory<>(map);
     }
 }
